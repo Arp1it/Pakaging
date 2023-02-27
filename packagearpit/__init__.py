26fe46f5227f048765de6df7c2ce625fe124a48c
Arpit <arpitjaiswal9526@gmail.com>
2023-02-27 11:50:58 +0530

    Commited

---
+++ packagearpit/__init__.py
@@ -0,0 +1,7 @@
+class Achha:
+    def __init__(self):
+        print("Constructorn is builded")
+
+    def func(self, number):
+        print("This is a arpit")
+        return number
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "packagearpit",
+version = "0.0.3",
+description = "This is a arpit package",
+long_description = "This a long description of packagearpit",
+author = "arpit",
+packages = ["packagearpit"],
+install_requires = [])
